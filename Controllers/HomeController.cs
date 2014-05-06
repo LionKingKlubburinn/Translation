e619d0e18e1f73aaa3e752a6664b5cd71264995d
gunnarpg13 <gunnarpg13@ru.is>
2014-05-06 15:41:14 +0000

    bætti við einar er kjáni
    því hann erða

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 //Bara smá test
-//meira testghdfghdrt
+//meira testghdfghdrthjyjfyjjjffj Einar er kjáni
 
 namespace Translation.Controllers
 {
