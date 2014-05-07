7a2eb1c2cdef4af5d4256d70d3463d41cba68e69
Einar Thor <einarthor13@ru.is>
2014-05-07 10:51:34 +0000

    comment og translation model

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -7,5 +7,9 @@ namespace Translation.Models
 {
     public class Comment
     {
+        public int ID { get; set; }
+        public String Username { get; set; }
+        public String CommentText { get; set; }
+        public DateTime CommentDate { get; set; }
     }
 }
