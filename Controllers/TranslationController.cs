61799d086e83188591177986de343c3096e03318
gillirabbi <gislirafn89@gmail.com>
2014-05-07 12:51:14 +0000

    View fyrir Controller
    Bjó til 2 basic view fyrir controllera:
    /Translation
    /User
    
    Einnig til view fyrir /Request

--- Controllers/TranslationController.cs
+++ Controllers/TranslationController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Translation.Controllers
 {
-    public class TranslationController
+    public class TranslationController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Translation.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
