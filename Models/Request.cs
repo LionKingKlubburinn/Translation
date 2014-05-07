6c6bfc0a4f905dcadcf4b058ad510f86d8a20d13
Einar Thor <einarthor13@ru.is>
2014-05-07 11:00:15 +0000

    Models

--- Models/Request.cs
+++ Models/Request.cs
@@ -7,5 +7,12 @@ namespace Translation.Models
 {
     public class Request
     {
+        public int ID { get; set; }
+        public int Upvote { get; set; }
+        public bool ForHardOfHearing { get; set; }
+        public String RequestBy { get; set; }
+        public String Name { get; set; }
+        public String Language { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
--- Models/Translation.cs
+++ Models/Translation.cs
@@ -7,5 +7,19 @@ namespace Translation.Models
 {
     public class Translation
     {
+        public int ID { get; set; }
+        public int DownloadCounter { get; set; }
+        public bool ForHardOfHearing { get; set; }
+        public bool Ready { get; set; }
+        public bool ColaberationAllowed { get; set; }
+        public String Name { get; set; }
+        public String Contributor { get; set; }
+        public String VideoType { get; set; }
+        public String VideoGenre { get; set; }
+        public String VideoDescription { get; set; }
+        public String Language { get; set; }
+        public String Picture { get; set; }
+        public String File { get; set; }
+        public DateTime CommentDate { get; set; }
     }
 }
