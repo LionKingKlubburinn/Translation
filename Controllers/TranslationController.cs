c40f2134e09716770ca1ef1bc030968bde5e8cdd
gunnarpg13 <gunnarpg13@ru.is>
2014-05-07 10:53:26 +0000

    Added Controllers

--- Controllers/TranslationController.cs
+++ Controllers/TranslationController.cs
@@ -2,29 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace Translation.Controllers
 {
-    public class TranslationController : Controller
+    public class TranslationController
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
