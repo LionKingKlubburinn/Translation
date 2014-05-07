6bd2fbd8f93ee6b4c5e3821400fc1755e8071f34
Einar Thor <einarthor13@ru.is>
2014-05-07 13:50:02 +0000

    Request Repo komið

--- Models/RequestRepository.cs
+++ Models/RequestRepository.cs
@@ -7,6 +7,49 @@ namespace Translation.Models
 {
     public class RequestRepository
     {
+        private static RequestRepository instance;
 
+        public static RequestRepository Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new RequestRepository();
+                return instance;
+            }
+        }
+
+        private List<Request> requests = null;
+
+        private RequestRepository()
+        {
+            this.requests = new List<Request>();
+            
+        }
+
+        public IEnumerable<Request> GetRequests()
+        {
+            var result = from r in requests
+                         orderby r.DateCreated ascending
+                         select r;
+            return result;
+        }
+
+        public void AddRequest(Request r)
+        {
+            int newID = 1;
+            if (requests.Count() > 0)
+            {
+                newID = requests.Max(x => x.ID) + 1;
+            }
+            r.DateCreated = DateTime.Now;
+            r.ForHardOfHearing = false;
+            r.ID = newID;
+            r.Language = "Strump";
+            r.Name = "Lion King";
+            r.RequestBy = 1; //TODO GetUser
+            r.Upvote = 0;
+
+        }
     }
 }
