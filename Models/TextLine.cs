e329acbe75482a0ccb75cc77471018b92057a929
eddapeturs <eddapetursdottir@gmail.com>
2014-05-07 12:23:06 +0000

    TextLine klasi
    Hér er Textline klasinn sem ég gerði, þarfnast yfirferðar

---
+++ Models/TextLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Translation.Models
+{
+    public class TextLine
+    {
+        public int ID { get; set; }
+        public string OriginalText1 { get; set; }
+        public string OriginalText2 { get; set; }
+        public string FirstLine { get; set; }
+        public string SecondLine { get; set; }
+        public string Translation { get; set; }
+        public string LastModUser { get; set; }
+        public DateTime LastModDate { get; set; }
+    }
+}
