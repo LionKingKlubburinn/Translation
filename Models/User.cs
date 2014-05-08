62be7030d7cd2940861fd785e486f31a29ebc67b
Einar Thor <einarthor13@ru.is>
2014-05-08 13:54:49 +0000

    User kominn inn

---
+++ Models/User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Translation.Models
+{
+    public class User : IdentityUser
+    {
+        public virtual MyUserInfo MyUserInfo { get; set; }
+    }
+
+    public class MyUserInfo
+    {
+        public int Id { get; set; }
+        public bool Admin { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Nationality { get; set; }
+        public string Image { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class MyDbContext : IdentityDbContext<User>
+    {
+        public MyDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        public System.Data.Entity.DbSet<MyUserInfo> UserInfo { get; set; }
+    }
+}
