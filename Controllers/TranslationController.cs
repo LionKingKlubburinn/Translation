078bec196fbc008806da0aa940a2425620ead7bb
gunnarpg13 <gunnarpg13@ru.is>
2014-05-08 15:20:46 +0000

    COMMIT BIATCHES

--- Controllers/TranslationController.cs
+++ Controllers/TranslationController.cs
@@ -9,29 +9,29 @@ namespace Translation.Controllers
 {
     public class TranslationController : Controller
     {
-        // GET /Translation/1
-        public ActionResult Index(int id = 1)
-        {
-            // Viewmodel, jeij
-            //int id = 1;
-            var model = new ViewModel();
-            model.TranslationItems = TranslationRepository.Instance.GetTranslation(id);
-            model.CommentItems = CommentRepository.Instance.GetComments();
+        //// GET /Translation/1
+        //public ActionResult Index(int id = 1)
+        //{
+        //    // Viewmodel, jeij
+        //    //int id = 1;
+        //    var model = new ViewModel();
+        //    model.TranslationItems = TranslationRepository.Instance.GetTranslation(id);
+        //    model.CommentItems = CommentRepository.Instance.GetComments();
 
-            return View(model);
-        }
+        //    return View(model);
+        //}
 
-        // POST /Translation/
-        [HttpPost]
-        public ActionResult Index(string Text, int id = 1)
-        {
-            CommentRepository.Instance.AddComment(Text);
-            var model = new ViewModel();
-            model.TranslationItems = TranslationRepository.Instance.GetTranslation(id);
-            model.CommentItems = CommentRepository.Instance.GetComments();
+        //// POST /Translation/
+        //[HttpPost]
+        //public ActionResult Index(string Text, int id = 1)
+        //{
+        //    CommentRepository.Instance.AddComment(Text);
+        //    var model = new ViewModel();
+        //    model.TranslationItems = TranslationRepository.Instance.GetTranslation(id);
+        //    model.CommentItems = CommentRepository.Instance.GetComments();
 
-            return View(model);
-        }
+        //    return View(model);
+        //}
 
         // GET /Translation/Read/
         public ActionResult Read()
@@ -50,7 +50,7 @@ namespace Translation.Controllers
             return View();
         }
 
-        public ActionResult View()
+        public ActionResult Index()
         {
             return View();
         }
