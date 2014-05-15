32d6717257d4ce18bd0ede2a6abb3508779aac92
arorahr <arora12@ru.is>
2014-05-15 13:55:01 +0000

    Laga beiðnir

--- DAL/TranslateInitializer.cs
+++ DAL/TranslateInitializer.cs
@@ -37,9 +37,9 @@ namespace Translation.DAL
 
             var requests = new List<Request>
             {
-                new Request { ID=1, Name="InABundercup", DateCreated=DateTime.Now, ForHardOfHearing=false, Language="utlansk", RequestByID="sgsf", Upvote=0},
-                new Request { ID=2, Name="SwingersClub 9", DateCreated=DateTime.Now, ForHardOfHearing=false, Language="utlansk", RequestByID="sgsf", Upvote=0},
-                new Request { ID=3, Name="Palli gone wild 4", DateCreated=DateTime.Now, ForHardOfHearing=false, Language="utlansk", RequestByID="sgsf", Upvote=0},
+                new Request { ID=1, Name="Noah", DateCreated=DateTime.Now, ForHardOfHearing=false, Language="Pólska", RequestByID="4", Upvote=0},
+                new Request { ID=2, Name="Spider Man 2", DateCreated=DateTime.Now, ForHardOfHearing=true, Language="Íslenska", RequestByID="3", Upvote=0},
+                new Request { ID=3, Name="The Other Woman", DateCreated=DateTime.Now, ForHardOfHearing=false, Language="utlansk", RequestByID="2", Upvote=0},
                 new Request { ID=4, Name="King Kong", DateCreated=DateTime.Now, ForHardOfHearing=true, Language="íslenska", RequestByID="1", Upvote=0},
                 new Request { ID=5, Name="Toy Story 3", DateCreated=DateTime.Now, ForHardOfHearing=false, Language="íslenska", RequestByID="sgsf", Upvote=0},
             };
